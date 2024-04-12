35c5fe1667a99979c12fba46928d9806a740ad4b
István Gergő <istvangergo96@gmail.com>
2024-04-12 10:39:16 +0200

    fix: added header to login and logout pages

--- templates/pages/belep.tpl.php
+++ templates/pages/belep.tpl.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handyman Searcher</title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+<body>
 <?php if(isset($row)) { ?>
     <?php if($row) { ?>
         <h1>Bejelentkezett:</h1>
@@ -10,3 +19,4 @@
 <?php if(isset($errormessage)) { ?>
     <h2><?= $errormessage ?></h2>
 <?php } ?>
+</body>
